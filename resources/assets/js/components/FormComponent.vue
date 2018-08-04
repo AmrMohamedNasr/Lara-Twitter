87b86c421b8df936372e8004a52ddf990ce2041d
Amr Mohamed Nasr <re13amr@gmail.com>
2018-08-04 17:47:03 +0200

    add form vue component for tweeting

---
+++ resources/assets/js/components/FormComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Share your thoughts</div>
+
+                <div class="card-body">
+                    <form>
+                        <div class="row justify-content-center">
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <textarea 
+                                        class="form-control" 
+                                        rows="2" cols="8" 
+                                        maxlength="130" 
+                                        required>
+                                    </textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <button class="btn btn-primary">
+                                        Share
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>   
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,11 @@
 
 @section('content')
 <div class="container">
+    <form-component></form-component>
+    <br>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+            Timeline
         </div>
     </div>
 </div>
