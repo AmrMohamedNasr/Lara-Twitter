0114c8d216b4cd538cb4cedb1b9a86c1881148b4
Amr Mohamed Nasr <re13amr@gmail.com>
2018-08-04 18:05:27 +0200

    add relations between the models of post and user

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+	protected $fillable = ['user_id', 'body'];
+    /**
+	* Relation to the user model.
+    **/
+    public function users() {
+    	return $this->belongsTo(User::class);
+    }
 }
